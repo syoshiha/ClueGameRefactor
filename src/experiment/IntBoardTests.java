64206043c44b3b49f1c27f1b16b180caf1709698
Jason Ziolo <jziolo@mymail.mines.edu>
2015-10-06 15:20:19 -0600

    Added testTargets0_3

--- src/experiment/IntBoardTests.java
+++ src/experiment/IntBoardTests.java
@@ -3,6 +3,7 @@ package experiment;
 import static org.junit.Assert.*;
 
 import java.util.LinkedList;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -46,9 +47,18 @@ public class IntBoardTests {
 	
 	@Test
 	public void testTargets0_3(){
-	
+		BoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<BoardCell> targets = board.getTargets(cell);
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
 	}
-	//test
+
 	@Test
 	public void testTargets1_4() {
 		
