275cb8827aa91d6aa27012df1e64b4b380509bb4
Hayden Scott <hascott@mymail.mines.edu>
2015-10-06 14:52:14 -0600

    fail tests half written

--- src/experiment/IntBoardTests.java
+++ src/experiment/IntBoardTests.java
@@ -1,17 +1,39 @@
 package experiment;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class IntBoardTests {
 	
-	/* @Before
+	@Before
 	public void setupBoard(){
 		IntBoard board = new IntBoard();
-		
-	} */
+	} 
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testAdjacency0(){
 		
-
+	}
+	
+	@Test
+	public void testAdjacency1(){
+		
+	}
+	
+	@Test
+	public void testAdjacency2(){
+		
+	}
+	
+	@Test
+	public void testTargets0_3(){
+	
+	}
+	
+	@Test
+	public void testTargets1_4() {
+		
+	}
 }
