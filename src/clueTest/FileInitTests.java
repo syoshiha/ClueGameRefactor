0c67795811e34a7079a488defe21977823e84521
Jason Ziolo <jziolo@mymail.mines.edu>
2015-10-08 14:52:05 -0600

    started test classes

---
+++ src/clueTest/FileInitTests.java
@@ -0,0 +1,14 @@
+package clueTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileInitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
