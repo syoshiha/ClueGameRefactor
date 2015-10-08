038667a105d5763e5b1230ea6dbf320851f306e2
Jason Ziolo <jziolo@mymail.mines.edu>
2015-10-08 14:40:47 -0600

    added exception, boardcell, doordirection skeleton

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,5 +1,21 @@
 package clueGame;
 
+@SuppressWarnings("serial")
 public class BadConfigFormatException extends Exception {
-
+	String message;
+	
+	public BadConfigFormatException(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return message;
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
+	}
+	
 }
