22039d1fb9e42f98e1858fe3ca86ee355efeeb1f
syoshiha <syoshiha@mymail.mines.edu>
2015-10-22 11:29:21 -0600

    Created variables and functions within Player and Computer Player classes

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -1,5 +1,16 @@
 package clueGame;
 
-public class Player {
+import com.sun.prism.paint.Color;
 
+public class Player {
+	private String playerName;
+	private int row;
+	private int column;
+	private Color color;
+
+	public Card disproveSuggestion(Solution suggestion) {
+		
+	}
+	
+	
 }
