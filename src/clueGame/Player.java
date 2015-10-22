b11ab3b4ad4b9e93c97cda2d1d0256149d17cfdd
erikkacsm <ebaker@mymail.mines.edu>
2015-10-22 11:34:28 -0600

    Added myCards and seenCards to Player class

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -7,6 +7,9 @@ public class Player {
 	private int row;
 	private int column;
 	private Color color;
+	
+	private Card myCards;
+	private Card seenCards;
 
 	public Card disproveSuggestion(Solution suggestion) {
 		return new Card();
